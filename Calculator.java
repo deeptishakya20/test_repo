80bb2cc6b89b680359233cb9ed96dbf192dbfece
Shakya <deepti.shakya@capgemini.com>
2019-03-04 15:10:45 +0530

    Edited calculator file

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,9 @@ public class Calculator{
             public static void main(String args[]){
 
                    System.out.println("Hello World!!!");
-                   System.out.println("Calculator Java Program");
-             }
+                   
+       
 
-             public static void addition(int number1, int number2){
 
-             System.out.println("Addition="+(number1+number2));
            }
 }
